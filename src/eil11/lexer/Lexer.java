91665bab6a5807fa45bc2ed21d4a6452f17fdb2d
Emilio Lopez <eil11@case.edu>
2017-09-20 22:23:00 -0400

    Add static initializer block for Pattern

--- src/eil11/lexer/Lexer.java
+++ src/eil11/lexer/Lexer.java
@@ -9,8 +9,21 @@ public class Lexer {
     private static Pattern tokenPatterns;
     private final Matcher matcher;
 
-    public Lexer (String input) {
+    static {
+        tokenPatterns = Pattern.compile(Token.Type.NOT.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.AND.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.OR.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.OPEN.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.CLOSE.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.ID.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.NUMBER.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.BINARYOP.getPattern());
+        tokenPatterns = Pattern.compile(Token.Type.WHITESPACE.getPattern());
+    }
 
+
+    public Lexer (String input) {
+//        tokenPatterns =
     }
 
     public Boolean hasNext() {
